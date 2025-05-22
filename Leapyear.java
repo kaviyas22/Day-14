a477ff59ec0ea99f189a3703ff5d6d2197cfa7a2
kaviyas22 <shreekaviya862@gmail.com>
2025-05-22 21:55:48 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leapyear;
+
+public class Leapyear {
+  
+    public static void main(String[] args) {
+        int a=2004;
+        if(a%4==0){
+        System.out.println("leap year:"+a);
+        }
+        // TODO code application logic here
+    }
+    
+}
